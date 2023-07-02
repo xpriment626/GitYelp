f30efb9cce755dc233724bec48d4d74c904cb763
Emmett Childs <emmettchilds@thegobospot.com>
2023-07-03 00:34:12 +0800

    feat: structure repo item data

---
+++ src/components/RepoItem.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { IRepoItem } from "../utils/types";
+import { View, Text } from "react-native"
+
+const RepoItem = (props: IRepoItem) => {
+    return (
+        <View>
+            <Text>{props.fullName}</Text>
+            <Text>{props.description}</Text>
+            <Text>{props.language}</Text>
+            <Text>{props.stargazersCount}</Text>
+            <Text>{props.forksCount}</Text>
+            <Text>{props.reviewCount}</Text>
+            <Text>{props.ratingAverage}</Text>
+        </View>
+    )
+}
+
+export default RepoItem
